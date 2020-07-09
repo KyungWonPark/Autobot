26f5f97e1986d0b09b2a0ea24ed63f13a1c23786
KyungWonPark <kw.park.person@gmail.com>
2020-07-09 15:27:06 +0900

    Added comment for examples/casting

--- examples/casting/main.go
+++ examples/casting/main.go
@@ -22,28 +22,51 @@ func main() {
 
 	pMat := unsafe.Pointer(&mat[0][0])
 	// Remember! slice IS A CONTAINER TYPE !
+	// Be aware of difference between Go array and slice
+	// Slice is a container type, that is wrapping Go array
 	//
 	// Slice is a struct, composed of:
 	// - len (INT, tells the length of a slice)
 	// - cap (INT, tells the capacity of a slice)
 	// - arr ([]ANY, underlying array that holds actual data)
 	//
-	// Therefore, &mat IS NOT ADDRESS OF first element in mat
+	// Therefore, &mat IS NOT AN ADDRESS OF first element in mat
 	// &mat[0][0] is the address of the first element !
 
+	// One way
 	for i := 0; i < 100; i++ {
-		// Use uintptr for pointer arithmetic
-		stride := uintptr(unsafe.Sizeof(int(0)))
-		index := uintptr(i)
+		// Read https://golang.org/pkg/unsafe/ for more
+
+		arr := *(*[100]int)(pMat)
+		// Note that we can dereference pMat as an ARRAY (which is just like C array, continuous static list of elements)
+		// but not as SLICE ! *(*[]int)(pMat) will result in error due to the reason explained above.
+		// If you can't understand, draw memory diagram and think again.
+
+		fmt.Printf("%d\n", arr[i])
+	}
+
+	// Another way
+	for i := 0; i < 100; i++ {
+		// uintptr is just uint, representing memory address
+		index := uintptr(i)                      // which is i
+		stride := uintptr(unsafe.Sizeof(int(0))) // which is 4 (bytes)
 
 		num := *(*int)(unsafe.Pointer(uintptr(pMat) + index*stride))
-		// get memory address by uintptr operation,
-		// then cast it into unsafe pointer,
-		// then cast it into *int,
+		// Convert pMat unsafe pointer to uintptr for pointer arithmetic,
+		// start from pMat, go index * stride bytes,
+		// convert that address into unsafe pointer,
+		// then cast it as an pointer pointing to integer data,
 		// then dereference it.
 
 		fmt.Printf("%d\n", num)
 	}
+
+	// Caution:
+	// Golang has garbage collection.
+	// If an array that your unsafe pointer is pointing gets GC'd,
+	// you'll have segmentation fault.
+	// So be cautious of variable scope
+	// Don't use pointer casting outside of data's lifespan.
 
 	return
 }
